66a83ccdc98b3fe5179985d998f9c137fc74164b
Taiye Salami <taiye41@gmail.com>
2020-04-24 05:52:37 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+
+    attr_accessor :name, :song
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(song_name)
+        Song.new(song_name).artist = self
+    end
+
+    def self.song_count
+        Song.all.count
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+end
---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+class Post
+
+    attr_accessor :title, :author
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
+    def author_name
+        if self.author == nil
+            nil
+        else author.name
+        end
+    end
+
+    
+
+end
